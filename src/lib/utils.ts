06f790f86c39ca65f0bc0724f13e4ea888541951
rikiTada <riki.tada777@gmail.com>
2026-03-26 14:56:22 +0900

    fix: コードの整形とsleep関数の追加

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+/**
+ * 処理を`ms`秒だけ一時停止する
+ */
+export const sleep = (ms: number) => {
+  return new Promise((r) => setTimeout(r, ms));
+};
