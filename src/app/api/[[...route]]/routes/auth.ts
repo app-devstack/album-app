fb9647ed228df0340f81dd6935d0a071d5113654
rikiTada <riki.tada777@gmail.com>
2026-04-01 11:42:15 +0900

    feat: 不要なサインインおよびサインアップのメール認証ロジックを削除

--- src/app/api/[[...route]]/routes/auth.ts
+++ src/app/api/[[...route]]/routes/auth.ts
@@ -1,60 +1,8 @@
-import type { User } from '@/db/schema';
 import { createApp } from '@/lib/api';
 import { auth } from '@/lib/auth/auth';
-import { authEmailSchema } from '@/lib/auth/auth-validation';
-import { getUserByEmail } from '@/lib/service/users';
-import { zValidator } from '@hono/zod-validator';
-
-const getUser = async (email: string): Promise<User | null> => {
-  try {
-    const dbUser = await getUserByEmail(email);
-    return dbUser;
-  } catch (error) {
-    console.error('Error fetching user by email:', error);
-    return null;
-  }
-};
 
 const router = createApp();
 
 export const authRouter = router
-  .get(
-    '/sign-in/email',
-    zValidator('json', authEmailSchema),
-    async (c, next) => {
-      const { email } = c.req.valid('json');
-      const user = await getUser(email);
-
-      if (!user) {
-        return c.json(
-          {
-            code: 'retry_login',
-            message: '入力内容を確認して、もう一度お試しください。',
-          },
-          403
-        );
-      }
-      await next();
-    }
-  )
-  .get(
-    '/sign-up/email',
-    zValidator('json', authEmailSchema),
-    async (c, next) => {
-      const { email } = c.req.valid('json');
-      const user = await getUser(email);
-
-      if (!user) {
-        return c.json(
-          {
-            code: 'retry_login',
-            message: '入力内容を確認して、もう一度お試しください。',
-          },
-          403
-        );
-      }
-      await next();
-    }
-  )
   .on(['GET', 'POST'], '/*', (c) => auth.handler(c.req.raw))
   .get('/ok', (c) => c.json({ status: 'ok' }));
